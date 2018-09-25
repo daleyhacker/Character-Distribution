f9884581e580ec47158bc98c1ffd614e35c36c22
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-09-25 18:29:20 -0400

    Updated from Brython Server: 9/25/2018 6:29:19 PM

--- distribution.py
+++ distribution.py
@@ -1,6 +1,6 @@
 """
 distribution.py
-Author: <your name here>
+Author: Patrick Daley   
 Credit: <list sources used, if any>
 
 Assignment:
@@ -36,3 +36,18 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+
+text = input("Please enter a string of text (the bigger the better): ")
+print('The distribution of characters in "' + text + '" is: ')
+
+#Makes text lowercase
+text = text.lower()
+
+a = text.count('a')
+
+
+print(a)
+
+
+
+
